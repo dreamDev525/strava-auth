3bff5eea23fb7a143837017f9e541b70aecd90a9
dreamDev525 <dreamdev525@gmail.com>
2022-11-27 08:21:45 +0600

    initialize

---
+++ api/callback.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const qs = require('qs');
+
+const redirectWithQueryString = require('../util/redirectWithQueryString');
+
+const stravaUrl = 'https://www.strava.com/oauth/';
+
+module.exports = async (req, res) => {
+  const { code } = req.query;
+
+  if (!code) {
+    return redirectWithQueryString(res, {
+      error: 'A response code is required in order to authorize.',
+    });
+  }
+  // axios api posting
+  const qsParams = {
+    code,
+    client_id: process.env.STRAVA_CLIENT_ID,
+    client_secret: process.env.STRAVA_CLIENT_SECRET,
+  };
+
+  return axios({
+    method: 'post',
+    url: `${stravaUrl}token?${qs.stringify(qsParams)}`,
+  })
+    .then(response => {
+      const data = qs.parse(response.data);
+
+      return redirectWithQueryString(res, data);
+    })
+    .catch(err => {
+      return redirectWithQueryString(res, err);
+    });
+};
